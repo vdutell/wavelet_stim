88662d4b731c45b01113da17833cbccfdf0e1ac0
Vasha <vashadutell@gmail.com>
2018-08-20 16:58:50 -0700

    spatiotemporal processing utilities

--- utils/fouriertools.py
+++ utils/fouriertools.py
@@ -4,7 +4,10 @@ import math
 
 import utils.imtools as imtools
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+import matplotlib.gridspec as gsp
 
 # from https://code.google.com/archive/p/agpy/downloads
 def azimuthalAverage(image, center=None):
@@ -138,6 +141,42 @@ def gen_temporal_ft(filtered_stim, filt, raw_stim, stim_fps, filt_name, fc):
     plt.legend()
     
     return(fq_plt)
+
+
+def gen_spatialtemporal_ft(filtered_stim, filt, raw_stim, stim_cpd, stimfps, filt_name, s_fc, t_fc):
+    
+    # Circular Averaged Power Spectrum - filtered_stim
+    filtered_ft = azimuthalAverage(np.abs(np.fft.fftshift(np.fft.fft2(filtered_stim)))**2)
+     # Circular Averaged Power Spectrum - raw_stim
+    raw_ft = azimuthalAverage(np.abs(np.fft.fftshift(np.fft.fft2(raw_stim)))**2)
+    # Circular Averaged Power Spectrum - filter
+    filt_ft = azimuthalAverage(filt**2)
+    
+    #normalize all to max 1
+    filtered_ft /= np.max(filtered_ft)
+    raw_ft /= np.max(raw_ft)
+    filt_ft /= np.max(filt_ft)
+    
+    # Get the Frequencies in CPD
+    cpds = np.linspace(0,stim_cpd,len(raw_ft))
+    
+    #plot them all
+    azm_plt = plt.figure(figsize = (8,6))
+    azm_plt = plt.loglog(cpds, filtered_ft,  '.', 
+                         label='filtered_stim')
+    azm_plt = plt.loglog(cpds, raw_ft, '.', label='raw_stim')
+    azm_plt = plt.loglog(cpds, filt_ft, '.', label='filter')
+    #azm_plt = plt.plot(cpds, filtered_ft, label='filtered_stim',)
+    #azm_plt = plt.plot(cpds, raw_ft, label='raw_stim')
+    #azm_plt = plt.plot(cpds, filt_ft, label='filter')
+    
+    azm_plt = plt.axvline(fc, c='k')
+    plt.xlabel('Frequency (cycles/deg)')
+    plt.ylabel('Power')
+    plt.title(f'Stim Fourier Spectra - {filt_name} Filter: Fc_  ={fc}')
+    plt.legend()
+    
+    return(azm_plt)
 
 
 def filt_cosine_step(f, fc):
@@ -317,3 +356,307 @@ def fft_temporal_lowpass(stim_in, fps_cutoff, stim_fps, filt_name='sharp', resca
         img_ifft = imtools.rescale_255(img_ifft)
     
     return img_ifft, mag, phase, filt, warn_flag
+
+def fft_spatiotemporal_lowpass(stim_in, cpd_cutoff, fps_cutoff, stim_cpd, stim_fps, filt_name='sharp', rescale=True):
+    '''
+    Lowpass filter an movie at a given frequency cuttoff using Fourier representation, for a given fps of the stimulus
+    
+    Args:
+        stim_in (3d numpy array):   stimluius movie (time, x, y)
+        cpd_cutoff (float): maximum cpd value present in output stim
+        fps_cutoff (float):  maximum fps value present in output stim
+        stim_cpd (flot):    CPD of stimulus (should be larger than cpd_cuttoff)
+        stim_fps (float):    FPS of stimulus (should be larger than fps_cuttoff)
+        filt_name (str):     define the type of filtering desired (sharp, cosine_step, gauss_step, gauss_taper)
+        
+    Returns:
+        stim (2d numpy array):   stimlulus image fourier filtered and no frequencies higher than cpd_cuttoff
+        mag (2d numpy array):   magnitude of stimulus
+        phase (2d numpy array): global phase angle of stimulus
+        filt (2d numpy array): fiter used to create filtered img
+        warn_flag (bool):   flag  if we had a warning in generating image
+    '''
+    
+    # warn flags are false by default
+    warn_flag_s = False
+    warn_flag_t = False
+    #make sure parameters make sense
+    if fps_cutoff > stim_fps/2:
+        warnings.warn('Cutoff FPS is higher than stimulus CPS')
+        warn_flag_s=True
+    #size of stim
+    stimf, stimx, stimy = np.shape(stim_in)
+        
+    #find ratio of cuttoff to max cpd so we know where to stop in fourier space
+    fft_len_fc_s = cpd_cutoff/(stim_cpd)
+    #find same ratio in fps
+    fft_len_fc_t = fps_cutoff/(stim_fps/2)
+    #get ft of stim
+    fft = np.fft.fftshift(np.fft.fftn(stim_in))
+    mag = np.abs(fft)
+    phase = np.angle(fft)
+    #calculate diameters (frequencies) of signal
+    xx, yy = np.meshgrid(np.linspace(-1, 1, stimx),
+                         np.linspace(-1, 1, stimy)) #THIS IS NOT VALID IF STIM IS NOT SQUARE111
+    fft_diameters_s = np.sqrt(xx**2 + yy**2)
+    fft_diameters_t = np.tile(np.abs(np.linspace(-1,1,stimf)),(stimx, stimy, 1)).T
+    
+    #calculate filter
+    if(filt_name=='sharp'):
+    # Anything greater than cpd_cutoff set to 0 in the mag
+        filt_s = (fft_diameters_s <= fft_len_fc_s) 
+        filt_t = (fft_diameters_t <= fft_len_fc_t) 
+        # pointwise multiply spatial and temporal filters to get full filter.
+        filt = np.multiply(filt_s, filt_t)
+        
+    elif(filt_name=='gauss_taper'):
+        #calculate sigma needed for HWHM value to be at fc
+        #sigma = fft_diameter_fc / (np.sqrt(-np.log(np.sqrt(0.5))))
+        #rescale to 100 so we don't get numerical instability
+        sigma_s = fft_len_fc_s*100000 / np.sqrt(-2*np.log(np.sqrt(0.5)))
+        filt_s = np.exp(-1.5*((fft_diameters_s*100000)/(sigma_s))**2)
+        sigma_t = fft_len_fc_t*100000 / np.sqrt(-2*np.log(np.sqrt(0.5)))
+        filt_t = np.exp(-1.5*((fft_diameters_s*100000)/(sigma_t))**2)
+        # pointwise multiply spatial and temporal filters to get full filter.
+        filt = np.multiply(filt_s, filt_t)
+        
+    elif(filt_name=='cosine_step'):
+        filt_s, warn_flag_s = filt_cosine_step(fft_diameters_s, fft_len_fc_s)
+        filt_t, warn_flag_t = filt_cosine_step(fft_diameters_t, fft_len_fc_t)
+        # pointwise multiply spatial and temporal filters to get full filter.
+        filt = np.multiply(filt_s, filt_t)
+        
+    elif(filt_name=='gauss_step'):
+        filt_s, warn_flag_s = filt_gauss_step(fft_diameters_s, fft_len_fc_s)
+        filt_t, warn_flag_t = filt_gauss_step(fft_diameters_t, fft_len_fc_t)
+        # pointwise multiply spatial and temporal filters to get full filter.
+        filt = np.multiply(filt_s, filt_t)
+    
+    else:
+        raise ValueError(f'{filt_name} is an unknown filtering type! Currently Supported Decompositions are \'sharp\', \'gauss_taper\', \'cosine_step\' and \'gauss_step\'.  Returning original Signal.')
+        filt = 1
+        
+    #multiply by arbitrary fourier filter
+    mag = np.multiply(mag,filt)
+    #print(mag[:,1,1])
+
+    # Reconstruct the movie using the inverse fourier transform
+    ifft = np.fft.fftshift(mag * np.exp(phase * 1.0j))
+    stim_ifft = np.fft.ifftn(ifft).real
+    
+    # Rescale to [0,255]
+    if(rescale):
+        stim_ifft = imtools.rescale_255(stim_ifft)
+        
+    warn_flag = warn_flag_s | warn_flag_t
+    
+    return stim_ifft, mag, phase, filt, warn_flag
+
+
+def make_3d_ft(movie, chunkshape, fps, ppd):
+    '''
+    Converts pixels from a trace to the corresponding pixels in a movie
+    
+    Parameters
+    ----------
+    movie:      3d numpy array definig movie for 3d fourier transform analysis.
+    chunkshape:  3ple defining shape (frames,x,y) of movie 'chunks'
+    
+    Returns:
+    --------
+    mftchunk
+    azmchunk
+    freqspace1d
+    freqspacefull
+    freqtime
+    
+    '''
+    
+    #If movie is in color, average three color channels to get greyscale
+    if(movie.ndim > 3):
+        movie = np.mean(movie,axis=3)
+    
+    #movie shape
+    #nframes = np.shape(movie)[0] #in frames
+    #framew = np.shape(movie)[1] #in pixels
+    #frameh = np.shape(movie)[2] #in pixels
+
+    
+    #if(chunklen%2 != 0 | chunklen<3):
+    #    raise Exception('Error, chunklen must be an even number > 2!')
+        
+    #nchunks = int(np.floor(len(movie[:,0,0])/chunklen))
+    
+    #remove mean (DC component)
+    #movie = movie - np.mean(movie)
+    
+    #take fourier transform over each fame
+    #fttimecube = np.fft.fft2(movie)
+    
+    chunks = cubify(movie, chunkshape)
+    del(movie)
+    ftchunks = []
+    
+    #break into chunks
+    for chunk in chunks:
+        #chunkmov = movie[chunk*chunklen:(chunk+1)*chunklen]
+        #remove mean (DC component)
+        chunk = chunk - np.mean(chunk)
+        #space FT for each chunk
+        #chunklist.append(np.fft.fftn(chunkmov,axes=[0]))
+        
+        #3d ft for each chunk
+        ftchunks.append(np.fft.fftn(chunk))
+        
+    del(chunks) #save space: we are done with raw chunks
+    chunklen = len(ftchunks)
+    
+    ftchunks=np.array(ftchunks)
+    
+    print(f'ftchunks: {ftchunks.shape}')
+    #take mean over all ftchunks to get one ftchunk
+    mftchunk = np.mean(ftchunks,axis=0)
+    #do fft shifts to make football
+    mftchunk = np.fft.fftshift(mftchunk)
+    print(f'mftchunk: {mftchunk.shape}')
+    
+    #array to hold azmaverage
+    #azmchunk = np.zeros([chunklen,int(np.floor(framew/2))-1], dtype=complex)
+    
+    azmchunk = []
+    ##spin to get mean
+    for f in range(np.shape(mftchunk)[0]):
+        azmchunk.append(azimuthalAverage(np.abs(mftchunk[f])))
+    del(mftchunk)
+    #only take positive side (real)
+    azmchunk = np.abs(np.array(azmchunk[int(chunkshape[0]/2):]))
+    print(f'azmchunk: {azmchunk.shape}')
+        
+    #azmchunk = (azmchunk[int(chunklen/2):] + azmchunk[int(chunklen/2):0:-1]) / 2
+      
+    #get the sampling rate for azmavgd
+    freqspace1d = np.fft.fftfreq(chunkshape[1], d=1./ppd)[0:int(np.floor(chunkshape[1]/2))-1]
+    #get the sampling rates
+    freqspacefull = np.fft.fftshift(np.fft.fftfreq(chunkshape[1], d=1./ppd))
+    freqtime = np.fft.fftshift(np.fft.fftfreq(chunkshape[0], d=1./fps))[int(chunkshape[0]/2):]
+    
+    #normalize the fft based on dx and dy
+    dspace = freqspace1d[1] - freqspace1d[0]
+    dtime = freqtime[1] - freqtime[0]
+    #azmchunk = azmchunk - np.mean(azmchunk)
+    azmchunk *= np.real(np.abs(dspace*dtime))
+    
+    
+    return(azmchunk, freqspace1d, freqtime)
+
+
+#def da_spatiotemporal_ft(amp_spectrum, fqspace, fqtime, nsamples = 7, figname='nameless', logscale=False):
+    
+def st_ft(stim):
+    # Amplitude Spectrum - filtered_stim
+    ft3d = np.abs(np.fft.fftshift(np.fft.fftn(stim)))
+    ft = []
+    for i, frame in enumerate(ft3d):
+        ft.append(azimuthalAverage(frame))
+    ft = np.array(ft)
+
+    #normalizeto max 1
+    ft /= np.max(ft)
+    
+    return(ft)
+    
+def da_spatiotemporal_ft(ft, stim_cpd, stim_fps, filt_name, fc_s, fc_t, save_fname, nsamples=5, logscale=False):
+    
+    figname = f'{filt_name}: Fc_s={fc_s}, Fc_t={fc_t}'
+    
+    ntfqs, nsfqs = np.shape(ft)
+    
+    # Get the frequencies in cpd
+    fqspace= np.linspace(0,stim_cpd, nsfqs)
+    # Get the Frequencies in fps (0 to nyquist)
+    fqtime = np.linspace(0,stim_fps/2, ntfqs)
+    
+    #colors for lines
+    spacesamplefqs_idx = np.linspace(fqspace[1],
+                                 fqspace[-1],
+                                 nsamples).astype(int)
+    timesamplefqs_idx = np.linspace(fqtime[1],
+                                fqtime[-1],
+                                nsamples).astype(int)
+
+    spacecolors = np.array(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])#[::-1]
+    timecolors = spacecolors
+    
+    #make a grid
+    fig = plt.figure(figsize=(10,6))
+    full_grid = gsp.GridSpec(2,3)
+    
+    #layout of subplots
+    grid_hm = gsp.GridSpecFromSubplotSpec(1,1,subplot_spec=full_grid[0:2,0:2])
+    grid_time = gsp.GridSpecFromSubplotSpec(1,1,subplot_spec=full_grid[1,2])
+    grid_space = gsp.GridSpecFromSubplotSpec(1,1,subplot_spec=full_grid[0,2])
+    
+    #heatmap
+    axes_hm = plt.subplot(grid_hm[0])
+    
+    # plot the correct spectrum (power/amplitude/psd)
+    # http://www.ldeo.columbia.edu/users/menke/research_notes/menke_research_note157.pdf
+    joint_power = np.abs(ft)**2
+    #joint_psd = 2/(len(fqspace)*len(fqtime)) * (np.abs(amp_spectrum)**2)
+    plot_spectrum = joint_power.T
+    #convert to db
+   # plot_spectrum /= np.max(plot_spectrum) #normalize so max val is zero (DB scaling)
+    plot_spectrum = np.log10(plot_spectrum)
+
+    hm = axes_hm.contourf(fqtime, fqspace, plot_spectrum,
+                         cmap='gray')
+    if(logscale):
+        axes_hm.set_xscale("log") 
+        axes_hm.set_yscale("log")
+    axes_hm.set_xlabel('Hz')
+    axes_hm.set_ylabel('cycles/deg')
+    axes_hm.set_title(f'{figname} Log Power') 
+    plt.colorbar(hm)
+
+    #add lines
+    for s in range(nsamples):
+        #lines in time
+        axes_hm.axvline(fqtime[timesamplefqs_idx[s]],c=timecolors[s],ls='-')
+        #lines in space
+        axes_hm.axhline(fqspace[spacesamplefqs_idx[s]],c=spacecolors[s],ls='--')
+
+    #spaceplot
+    axes_space = plt.subplot(grid_space[0])
+    for i, tf_idx in enumerate(timesamplefqs_idx):
+        axes_space.semilogx(fqspace, plot_spectrum[:,tf_idx],
+                        label='{0:0.1f} Hz'.format(fqtime[tf_idx]),
+                        c=timecolors[i])
+    
+    #axes_space.plot(fqspace[1:],1e-80/(fqspace[1:]),c='black') # 1/f line
+    #axes_space.plot(fqspace[1:],1e-80/(fqspace[1:]**2),c='black') # 1/f^2 line
+    
+    axes_space.set_title('Spatial Frequency')
+    axes_space.set_xlabel('cpd')
+    axes_space.set_ylabel(f'{figname} Log Power')
+    axes_space.set_xlim(fqspace[1],fqspace[-1])
+    axes_space.xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%d'))
+    axes_space.legend(fontsize=8)
+
+    #timeplot
+    axes_time = plt.subplot(grid_time[0])
+    for i, sf_idx in enumerate(spacesamplefqs_idx):
+        axes_time.semilogx(fqtime, plot_spectrum[sf_idx,:],
+                       label='{0:0.1f} cpd'.format(fqspace[sf_idx]),
+                       c=spacecolors[i])
+    #axes_time.plot(fqtime[1:],1e4/(fqtime[1:]),c='black') # 1/f line
+    #axes_time.plot(fqtime[1:],1e7/(fqtime[1:]**2),c='black') # 1/f^2 line
+    axes_time.set_title('Temporal Frequency')
+    axes_time.set_xlabel('Hz')
+    axes_time.set_ylabel(f'{figname} Log Power') 
+    axes_time.set_xlim(fqtime[1],fqtime[-1])
+    axes_time.xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%d'))
+    axes_time.legend(fontsize=8)
+
+    plt.tight_layout()
+    
+    plt.savefig(save_fname)
--- utils/getstim.py
+++ utils/getstim.py
@@ -94,7 +94,7 @@ def generate_spatial_filtered_stims(stim, stimdeg, cutoffs, filt='fourier_sharp'
     stim_cpd = (stimpx_w/2)/stimdeg
     
     #save our raw stim
-    stim_fname = f'{stim_outfolder}{stim_type}_raw_{int(stim_cpd)}cpd.png'
+    stim_fname = f'{stim_outfolder}{stim_type}_{int(stim_cpd)}cpdc_raw.png'
     imwtools.writestim(stim, stim_fname)
     # create our raw stim's FT
     stim_ft = ftools.gen_azm_spatial_ft(stim, np.ones_like(stim), stim, stim_cpd, filt, int(stim_cpd))
@@ -104,19 +104,16 @@ def generate_spatial_filtered_stims(stim, stimdeg, cutoffs, filt='fourier_sharp'
     print(f'Wrote {stim_ft_fname}')
 
     # loop through cuttoff frequencies and filter
-    for idx, cut in enumerate(cutoffs):
+    for cut in cutoffs:
         filt_stim, stim_mag, stim_phase, stim_filter, warn_flag = ftools.fft_lowpass(stim, cut, stim_cpd, filt)
         stim_filt_ft = ftools.gen_azm_spatial_ft(filt_stim, stim_filter, stim, stim_cpd, filt, cut)
         # if we had a warning during generating the image, reflect in image filename
+        stim_fname = f'{stim_outfolder}{stim_type}_{filt}_{int(cut)}cpd'
         if(warn_flag):
-            stim_fname = f'{stim_outfolder}{stim_type}_{filt}_{int(cut)}cpd_warn.png'
-            stim_ft_fname = f'{ft_outfolder}{stim_type}{filt}_{int(cut)}cpd_ft_warn.png'
-        else:
-            stim_fname = f'{stim_outfolder}{stim_type}_{filt}_{int(cut)}cpd.png'
-            stim_ft_fname = f'{ft_outfolder}{stim_type}_{filt}_{int(cut)}cpd_ft.png'
+            stim_fname = stim_fname + '_warn'
         # write to disk
-        imwtools.writestim(filt_stim, stim_fname)
-        imwtools.writeplot(stim_filt_ft, stim_ft_fname)
+        imwtools.writestim(filt_stim, f'{stim_fname}.png')
+        imwtools.writeplot(stim_filt_ft, f'{stim_dir}/ft.png')
         print(f'Wrote {stim_fname}; {stim_ft_fname}')
         
     return()
@@ -151,22 +148,63 @@ def generate_temporal_filtered_stims(stim, stimfps, cutoffs, filt='fourier_sharp
 #    print(f'Wrote {stim_ft_fname}')
 
     # loop through cuttoff frequencies and filter
-    for idx, cut in enumerate(cutoffs):
-        filt_stim, stim_mag, stim_phase, stim_filter, warn_flag = ftools.fft_temporal_lowpass(stim, cut, stimfps, filt)
+    for cut in cutoffs:
+        filt_stim, stim_mag, stim_phase, stim_filter, warn_flag = ftools.fft_spatiotemporal_lowpass(stim, cut, stimfps, filt)
         stim_filt_ft = ftools.gen_temporal_ft(filt_stim, stim_filter, stim, stimfps, filt, cut)
         # if we had a warning during generating the image, reflect in image filename
+        stim_dir = f'{stim_outfolder}{stim_type}_{int(stimfps)}fps_{filt}_{int(cut)}fps'
         if(warn_flag):
-            stim_dir = f'{stim_outfolder}{stim_type}_{filt}_{int(stimfps)}fps_{int(cut)}cut_warn/'
-        else:
-            stim_dir = f'{stim_outfolder}{stim_type}_{filt}_{int(stimfps)}fps_{int(cut)}cut/'
+            stim_dir = stim_dir + '_warn'
         #save our fitlered stim
         pathlib.Path(stim_dir).mkdir(exist_ok=True)
         for i, frame in enumerate(filt_stim):
-            stim_fname = stim_dir + f'frame_{i+1}.png'
+            stim_fname = stim_dir + f'/frame_{i+1}.png'
             imwtools.writestim(frame, stim_fname)
         #save our fourier transform
-        imwtools.writeplot(stim_filt_ft, stim_dir + 'ft.png')
+        imwtools.writeplot(stim_filt_ft, f'{stim_dir}/ft.png')
 
         print(f'Wrote {stim_fname}')
+        
+    return()
+
+def generate_spatiotemporal_filtered_stims(stim, stimcpd, spatial_cutoffs,
+                                            stimfps, temporal_cutoffs, filt='fourier_sharp', stim_type='stepfun'):
+    
+    '''
+    Generate filtered stimlui at the given cuttoffos with a given filter
+    mode (See PIL modes: https://pillow.readthedocs.io/en/3.1.x/handbook/concepts.html#modes)
+    '''
+    
+    stim_outfolder = 'filtered_stims/spatiotemporal/'
+    stimlen_frames, stimpx_w, stimpx_h = np.shape(stim)
+
+    #save our raw stim
+    stim_dir = f'{stim_outfolder}{stim_type}_{int(stimcpd)}cpd_{int(stimfps)}fps_raw'
+    pathlib.Path(stim_dir).mkdir(exist_ok=True)
+    for i, frame in enumerate(stim):
+        imwtools.writestim(frame, f'{stim_dir}/frame_{i+1}.png')
+    # create our raw stim's FT
+    ftools.da_spatiotemporal_ft(ftools.st_ft(stim), stimcpd, stimfps, 'raw', stimcpd, stimfps, f'{stim_dir}/ft_raw.png')
+
+    # loop through cuttoff frequencies and filter
+    for s_cut in spatial_cutoffs:
+        for t_cut in temporal_cutoffs:
+            filt_stim, stim_mag, stim_phase, stim_filter, warn_flag = ftools.fft_spatiotemporal_lowpass(stim, s_cut, t_cut, stimcpd, stimfps, filt)
+            #create fourier transform of filtered stim
+            #stim_filt_ft = ftools.gen_spatiotemporal_ft(filt_stim, stim_filter, stim, stimcpd, stimfps, filt, s_cut, t_cut)
+            # if we had a warning during generating the image, reflect in image filename,
+            stim_dir = f'{stim_outfolder}{stim_type}_{int(stimcpd)}cpd_{int(stimfps)}fps_{filt}_{int(s_cut)}cpd_{int(t_cut)}fps'
+            if(warn_flag):
+                stim_dir = stim_dir + '_warn'
+            #save our fitlered stim
+            pathlib.Path(stim_dir).mkdir(exist_ok=True)
+            for i, frame in enumerate(filt_stim):
+                stim_fname = stim_dir + f'/frame_{i+1}.png'
+                imwtools.writestim(frame, stim_fname)
+            #save our fourier transform
+            ftools.da_spatiotemporal_ft(ftools.st_ft(filt_stim), stimcpd, stimfps, 'filtered_stim', s_cut, t_cut, f'{stim_dir}/ft_filtered_stim.png')
+            ftools.da_spatiotemporal_ft(filt, stimcpd, stimfps, 'filter', s_cut, t_cut, f'{stim_dir}/ft_filt.png')
+
+            print(f'Wrote {stim_fname}')
         
     return()
